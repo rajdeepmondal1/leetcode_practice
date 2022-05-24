78678a7ba1f2a24ae1d9b55bbe780ed35e6d80d8
Rajdeep Mondal <sachinrajdeeptendulkar@gmail.com>
2022-05-24 16:19:35 +0530

    added 49_group_anagrams.py

---
+++ 49_group_anagrams.py
@@ -0,0 +1,18 @@
+# 49 - Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+from collections import defaultdict
+
+
+def groupAnagrams(strs):
+    result = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for letter in s:
+            count[ord(letter) - ord('a')] += 1
+        result[tuple(count)].append(s)
+    return result.values()
+
+
+print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(groupAnagrams([""]))
+print(groupAnagrams(["a"]))
